c7261fd9550b0cac00ad4631c479c157a334df65
Dmitry Masukevich <masukevich@gmail.com>
2017-11-03 11:12:35 +0300

    Add database initializer, refactor reuire in environment

--- config/environment.rb
+++ config/environment.rb
@@ -6,22 +6,31 @@ require 'bcrypt'
 require 'sequel'
 require 'grape'
 require 'grape-entity'
-
-# TODO: this is wrong
-ENV['RACK_ENV'] ||= 'development'
-ENV['DATABASE_URL']="sqlite://bs_#{ENV['RACK_ENV']}.sqlite3"
-DB = Sequel.connect(ENV['DATABASE_URL'])
-
-Dir["#{File.expand_path('../initializers/*.rb', __FILE__)}"].each {|f| require f}
-
-Dir["#{File.expand_path('../../app/models/mixins/*.rb', __FILE__)}"].each {|f| require f}
-Dir["#{File.expand_path('../../app/models/*.rb', __FILE__)}"].each {|f| require f}
-
 require 'music_brainz/wrapper'
 
-Dir["#{File.expand_path('../../app/api/entities/*.rb', __FILE__)}"].each {|f| require f}
-Dir["#{File.expand_path('../../app/api/endpoints/*.rb', __FILE__)}"].each {|f| require f}
-Dir["#{File.expand_path('../../app/api/*.rb', __FILE__)}"].each {|f| require f}
+INITIALIZERS =
+  %w(
+    ../initializers/*.rb
+  )
+MODELS =
+  %w(
+    ../../app/models/mixins/*.rb
+    ../../app/models/*.rb
+  )
+API =
+  %w(
+    ../../app/api/entities/*.rb
+    ../../app/api/endpoints/*.rb
+    ../../app/api/*.rb
+  )
+LIB =
+  %w(
+    ../../lib/*.rb
+  )
 
-Dir["#{File.expand_path('../../lib/*.rb', __FILE__)}"].each {|f| require f}
+REQUIRE_ORDER = INITIALIZERS + MODELS + API + LIB
+
+REQUIRE_ORDER.each do |relative_path|
+  Dir["#{File.expand_path(relative_path, __FILE__)}"].each {|f| require f}
+end
 
